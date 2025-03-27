add27d67f2732914e45839250c5cfce1d70a7d32
German <German2025@gmail.com>
2025-03-27 16:31:02 -0500

    UPDATE dialect options

--- database/sequelize.js
+++ database/sequelize.js
@@ -4,7 +4,19 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 async function connectSequelize() {
